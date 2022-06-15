5961294b9a826254d83eb0a91a828f22d5dbe8a9
evertonmessias <everton.messias@gmail.com>
2022-06-15 17:43:00 -0300

    15-06-2022_17:43:00

---
+++ wp-content/themes/eos_theme/page-agillis.php
@@ -0,0 +1,179 @@
+<?php get_header(); ?>
+<!-- ======= Hero Section ======= -->
+<section id="hero">
+	<div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+		<ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+		<div class="carousel-inner" role="listbox">
+
+			<!-- Slide 1 -->
+			<div class="carousel-item active">
+				<video autoplay muted loop class="vdesk">
+					<source src="<?php echo SITEPATH; ?>assets/img/slide/home-1.webm" type="video/webm">
+				</video>
+				<!--<video autoplay muted loop class="vmobi">
+					<source src="<?php //echo SITEPATH; 
+									?>assets/img/slide/home-1_mobile.webm" type="video/webm">
+				</video>-->
+				<div class="carousel-container">
+					<div class="container">
+						<h2 class="center animate__animated animate__fadeInDown">AGILLIS</h2>
+						<br>
+						<hr class="bar"><br>
+						<h4 class="Center center animate__animated animate__fadeInUp">Tecnologia e simplicidade na obtenção<br>de consumo de água</h4>
+						<p class="Center center animate__animated animate__fadeInUp">Ferramenta completa para leitura, faturamento, impressão e<br>entrega simultânea de contas</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<!--<a class="carousel-control-prev" href="/#heroCarousel" role="button" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+		</a>
+
+		<a class="carousel-control-next" href="/#heroCarousel" role="button" data-bs-slide="next">
+			<span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+		</a>-->
+
+	</div>
+</section><!-- End Hero -->
+
+<main id="main">
+
+	<!-- ======= Clients Section ======= -->
+	<section id="clients" class="clients">
+		<div class="container" data-aos="zoom-in">
+			<br><br>
+			<h2 class="center">Alguns dos nossos clientes:</h2>
+			<br><br>
+			<div class="clients-slider swiper">
+				<div class="swiper-wrapper align-items-center">
+					<div class="swiper-slide"><img src="<?php echo SITEPATH; ?>assets/img/clients/client-1.png" class="img-fluid" alt=""></div>
+					<div class="swiper-slide"><img src="<?php echo SITEPATH; ?>assets/img/clients/client-2.png" class="img-fluid" alt=""></div>
+					<div class="swiper-slide"><img src="<?php echo SITEPATH; ?>assets/img/clients/client-3.png" class="img-fluid" alt=""></div>
+					<div class="swiper-slide"><img src="<?php echo SITEPATH; ?>assets/img/clients/client-4.png" class="img-fluid" alt=""></div>
+				</div>
+				<div class="swiper-pagination"></div>
+			</div>
+
+			<br><br>
+			<div class="section-title">
+				<h2>Reduza os erros de leitura e aumente<br>a produtividade de seu agente de campo</h2>
+				<br>
+				<p class="center">O Agillis realiza todo o processo de leitura, cálculo e emissão da fatura.<br>Com muita praticidade e precisão, assegura o cumprimento de todo o processo de<br>obtenção de leitura e faturamento, aumentando a produtividade do agente de<br>campo e diminuindo custos.</p>
+			</div>
+			<br><br>
+
+		</div>
+	</section><!-- End Clients Section -->
+
+
+
+	<!-- ======= Tabs Section ======= -->
+	<section class="tabs section-bg adark1">
+		<div class="container" data-aos="fade-up">
+			<div class="row">
+				<div class="col-lg-6 col1">
+					<div class="section-title">
+						<h4>Gerenciamento de leituras</h4>
+						<hr class="bar"><br>
+						<p class="justify">O Agillis gerencia por completo todas as fases do processo. Desde a geração da leitura até a atualização dos dados da conta no sistema gestor da empresa.</p>
+					</div>
+				</div>
+				<div class="col-lg-6 col2">
+					<img src="<?php echo SITEPATH; ?>assets/img/pages/laptop3.png" class="img-fluid" alt="">
+				</div>
+			</div>
+		</div>
+	</section><!-- End Tabs Section -->
+
+
+
+	<!-- ======= Tabs Section ======= -->
+	<section class="tabs section-bg alight">
+		<div class="container" data-aos="fade-up">
+			<div class="row">
+				<div class="col-lg-6 col1">
+					<img src="<?php echo SITEPATH; ?>assets/img/pages/laptop4.png" class="img-fluid" alt="">
+				</div>
+				<div class="col-lg-6 col2">
+					<div class="section-title">
+						<h4>Fotos de leituras e concorrências</h4>
+						<hr class="bar"><br>
+						<p class="justify">Com o Agillis você tira fotos das leituras e ocorrências de campo, dando transparência ao processo de obtenção de leituras e faturamento.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section><!-- End Tabs Section -->
+
+
+	<!-- ======= Tabs Section ======= -->
+	<section class="tabs section-bg adetail">
+		<div class="container" data-aos="fade-up">
+
+			<div class="row">
+				<div class="col-lg-2"></div>
+				<div class="col-lg-4 col">
+					<i class="ri-home-line"></i><br><br><br>
+					<h4>Tecnologia Rebusta</h4>
+					<hr class="barr"><br>
+					<p class="justify">Software confiável e estável, 30 anos de mercado, sendo regularmente evoluído para melhor atender nossos clientes. Desenvolvido em arquitetura de multicamadas e com processo ágil de implementação.</p>
+				</div>
+				<div class="col-lg-4 col">
+					<i class="ri-computer-line"></i><br><br><br>
+					<h4>Know How</h4>
+					<hr class="barr"><br>
+					<p class="justify">Sabemos como e porque fazer, isso tonar a solução uma ferramenta especializada e inovadora, capaz de aumentar consideravelmente a eficácia dos processos.</p>
+				</div>
+				<div class="col-lg-2"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-2"></div>
+				<div class="col-lg-4 col">
+					<i class="ri-archive-line"></i><br><br><br>
+					<h4>Gestão de Informação</h4>
+					<hr class="barr"><br>
+					<p class="justify">Visibilidade de informações. Saiba com exatidão dados cruciais para o seu negócio. Dessa forma, planeje com mais assertividade e alcance maior performance.</p>
+				</div>
+				<div class="col-lg-4 col">
+					<i class="ri-calendar-check-fill"></i><br><br><br>
+					<h4>Informações Compartilhadas</h4>
+					<hr class="barr"><br>
+					<p class="justify">Compartilhe informações entre sua equipe de maneira rápida e automatizada. Reduzindo custos e simplificando os processos.</p>
+				</div>
+				<div class="col-lg-2"></div>
+			</div>
+
+			<div class="row">
+				<div class="col-lg-2"></div>
+				<div class="col-lg-4 col">
+					<i class="ri-chat-3-line"></i><br><br><br>
+					<h4>Interações</h4>
+					<hr class="barr"><br>
+					<p class="justify">Fácil integração a outros sistemas, por intermédio de componentes Web Service ou por rotinas desenvolvidas para este fim.</p>
+				</div>
+				<div class="col-lg-4 col">
+					<i class="ri-layout-2-line"></i><br><br><br>
+					<h4>100% costumizável</h4>
+					<hr class="barr"><br>
+					<p class="justify">Você pode personalizar o sistema com os relatórios e rotinas que mais se adequam ao seu uso. Aumentando assim sua produtividade e melhorando a usabilidade.</p>
+				</div>
+				<div class="col-lg-2"></div>
+			</div>
+
+		</div>
+	</section><!-- End Tabs Section -->
+
+
+	<!-- ======= Tabs Section ======= -->
+	<section class="tabs section-bg adark2">
+	</section><!-- End Tabs Section -->
+
+
+
+</main><!-- End #main -->
+<?php get_footer(); ?>
